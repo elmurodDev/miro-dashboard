82ca05f3c6d6ed712a1a3ca4eb394fd54edf02f0
elmurodDev <elmurodorinboyev@gmail.com>
2024-06-11 18:46:57 +0500

    feat: add chart to the project

--- src/app/result/page.js
+++ src/app/result/page.js
@@ -19,11 +19,12 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table"
+import {Overview} from "@/components/common/Overview";
 
 const Page = () => {
   return (
     <div>
-      <Tabs defaultValue="week">
+      <Tabs defaultValue="calendar">
         <TabsList>
           <TabsTrigger value="calendar" className={"flex items-center gap-2"}>
             <IoCalendarOutline className={"icon-sm"}/>
@@ -34,7 +35,7 @@ const Page = () => {
           <TabsTrigger value="week">7 days</TabsTrigger>
         </TabsList>
         <TabsContent value="calendar">
-          <div className="grid gap-4 md:grid-cols-2 md:gap-8 lg:grid-cols-4">
+          <div className="grid gap-4 md:grid-cols-2 md:gap-8 lg:grid-cols-4 mt-5">
             <Card x-chunk="dashboard-01-chunk-0">
               <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
                 <CardTitle className="text-sm font-medium">
@@ -90,89 +91,133 @@ const Page = () => {
           </div>
 
           <div className={"grid grid-cols-12 mt-5 gap-6"}>
+            <Card className="col-span-6">
+              <CardHeader>
+                <CardTitle>Overview</CardTitle>
+              </CardHeader>
+              <CardContent className="pl-2">
+                <Overview/>
+              </CardContent>
+            </Card>
             <Card x-chunk="dashboard-05-chunk-3" className={"col-span-6"}>
-              <CardContent className={"p-1"}>
+              <CardContent className={"p-0"}>
                 <Table className={"text-xs"}>
                   <TableHeader>
                     <TableRow>
                       <TableHead>#</TableHead>
-                      <TableHead className="hidden sm:table-cell">Screens</TableHead>
-                      <TableHead className="hidden sm:table-cell">Screen views</TableHead>
-                      <TableHead className="hidden md:table-cell">Exits</TableHead>
+                      <TableHead>Screens</TableHead>
+                      <TableHead>Screen views</TableHead>
+                      <TableHead>Exits</TableHead>
                       <TableHead>Drop-off rate</TableHead>
                     </TableRow>
                   </TableHeader>
                   <TableBody className={"p-0"}>
                     <TableRow>
-                      <TableCell className="hidden sm:table-cell">1</TableCell>
+                      <TableCell>1</TableCell>
                       <TableCell>
                         <div className="font-medium">start</div>
                       </TableCell>
-                      <TableCell className="hidden sm:table-cell">13</TableCell>
-                      <TableCell className="hidden md:table-cell">9</TableCell>
+                      <TableCell>13</TableCell>
+                      <TableCell>9</TableCell>
                       <TableCell>69%</TableCell>
                     </TableRow>
                     <TableRow>
-                      <TableCell className="hidden sm:table-cell">2</TableCell>
+                      <TableCell>2</TableCell>
                       <TableCell>
                         <div className="font-medium">marketing-channels</div>
                       </TableCell>
-                      <TableCell className="hidden sm:table-cell">6</TableCell>
-                      <TableCell className="hidden md:table-cell">3</TableCell>
+                      <TableCell>6</TableCell>
+                      <TableCell>3</TableCell>
                       <TableCell>50%</TableCell>
                     </TableRow>
                     <TableRow>
-                      <TableCell className="hidden sm:table-cell">1</TableCell>
+                      <TableCell>1</TableCell>
                       <TableCell>
                         <div className="font-medium">start</div>
                       </TableCell>
-                      <TableCell className="hidden sm:table-cell">13</TableCell>
-                      <TableCell className="hidden md:table-cell">9</TableCell>
+                      <TableCell>13</TableCell>
+                      <TableCell>9</TableCell>
                       <TableCell>69%</TableCell>
                     </TableRow>
                     <TableRow>
-                      <TableCell className="hidden sm:table-cell">2</TableCell>
+                      <TableCell>2</TableCell>
                       <TableCell>
                         <div className="font-medium">marketing-channels</div>
                       </TableCell>
-                      <TableCell className="hidden sm:table-cell">6</TableCell>
-                      <TableCell className="hidden md:table-cell">3</TableCell>
+                      <TableCell>6</TableCell>
+                      <TableCell>3</TableCell>
                       <TableCell>50%</TableCell>
                     </TableRow>
                     <TableRow>
-                      <TableCell className="hidden sm:table-cell">1</TableCell>
+                      <TableCell>1</TableCell>
                       <TableCell>
                         <div className="font-medium">start</div>
                       </TableCell>
-                      <TableCell className="hidden sm:table-cell">13</TableCell>
-                      <TableCell className="hidden md:table-cell">9</TableCell>
+                      <TableCell>13</TableCell>
+                      <TableCell>9</TableCell>
                       <TableCell>69%</TableCell>
                     </TableRow>
                     <TableRow>
-                      <TableCell className="hidden sm:table-cell">2</TableCell>
+                      <TableCell>2</TableCell>
                       <TableCell>
                         <div className="font-medium">marketing-channels</div>
                       </TableCell>
-                      <TableCell className="hidden sm:table-cell">6</TableCell>
-                      <TableCell className="hidden md:table-cell">3</TableCell>
+                      <TableCell>6</TableCell>
+                      <TableCell>3</TableCell>
                       <TableCell>50%</TableCell>
                     </TableRow>
                     <TableRow>
-                      <TableCell className="hidden sm:table-cell">1</TableCell>
+                      <TableCell>1</TableCell>
                       <TableCell>
                         <div className="font-medium">start</div>
                       </TableCell>
-                      <TableCell className="hidden sm:table-cell">13</TableCell>
-                      <TableCell className="hidden md:table-cell">9</TableCell>
+                      <TableCell>13</TableCell>
+                      <TableCell>9</TableCell>
                       <TableCell>69%</TableCell>
                     </TableRow>
                     <TableRow>
-                      <TableCell className="hidden sm:table-cell">2</TableCell>
+                      <TableCell>2</TableCell>
                       <TableCell>
                         <div className="font-medium">marketing-channels</div>
                       </TableCell>
-                      <TableCell className="hidden sm:table-cell">6</TableCell>
-                      <TableCell className="hidden md:table-cell">3</TableCell>
+                      <TableCell>6</TableCell>
+                      <TableCell>3</TableCell>
+                      <TableCell>50%</TableCell>
+                    </TableRow>
+                    <TableRow>
+                      <TableCell>2</TableCell>
+                      <TableCell>
+                        <div className="font-medium">marketing-channels</div>
+                      </TableCell>
+                      <TableCell>6</TableCell>
+                      <TableCell>3</TableCell>
+                      <TableCell>50%</TableCell>
+                    </TableRow>
+                    <TableRow>
+                      <TableCell>2</TableCell>
+                      <TableCell>
+                        <div className="font-medium">marketing-channels</div>
+                      </TableCell>
+                      <TableCell>6</TableCell>
+                      <TableCell>3</TableCell>
+                      <TableCell>50%</TableCell>
+                    </TableRow>
+                    <TableRow>
+                      <TableCell>2</TableCell>
+                      <TableCell>
+                        <div className="font-medium">marketing-channels</div>
+                      </TableCell>
+                      <TableCell>6</TableCell>
+                      <TableCell>3</TableCell>
+                      <TableCell>50%</TableCell>
+                    </TableRow>
+                    <TableRow>
+                      <TableCell>2</TableCell>
+                      <TableCell>
+                        <div className="font-medium">marketing-channels</div>
+                      </TableCell>
+                      <TableCell>6</TableCell>
+                      <TableCell>3</TableCell>
                       <TableCell>50%</TableCell>
                     </TableRow>
                   </TableBody>
